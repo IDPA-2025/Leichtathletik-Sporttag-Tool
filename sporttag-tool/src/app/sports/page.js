9746cdd79005b399c4c51fcead244796a549c42b
klionCH <sven.luebcke@lernende.bbw.ch>
2025-03-12 22:37:36 +0100

    styling class

--- sporttag-tool/src/app/sports/page.js
+++ sporttag-tool/src/app/sports/page.js
@@ -2,22 +2,19 @@
 
 import Link from "next/link";
 
-
 export default function sports() {
+    const square = "bg-blue-800 rounded-lg w-[min(25vw,100px)] md:w-[30%] lg:w-[20%] aspect-square";
     return (
-        <div className="wrapper-container">
-            <div className="transparent-container">
+        <div className="wrapper-container h-screen">
+            <div className="transparent-container h-full">
                 <div className="h-1/5 bg-cyan-300 w-full">
-
                 </div>
-                <div className="flex items-center justify-around flex-wrap bg-amber-400 w-full h-4/5">
-                    <div className="bg-blue-800 w-56 aspect-square"></div>
-                    <div className="bg-blue-800 w-56 aspect-square"></div>
-                    <div className="bg-blue-800 w-56 aspect-square"></div>
-                    <div className="bg-blue-800 w-56 aspect-square"></div>
-                    <div className="bg-blue-800 w-56 aspect-square"></div>
-
-
+                <div className="flex h-4/5 bg-amber-600 w-full justify-center items-center flex-wrap gap-5">
+                    <div className={square}></div>
+                    <div className={square}></div>
+                    <div className={square}></div>
+                    <div className={square}></div>
+                    <div className={square}></div>
                 </div>
             </div>
         </div>
